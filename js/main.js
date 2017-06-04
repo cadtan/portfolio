cba8d0686b5182be5d1ce6ff8e40f3cb1d73c046
Caden Tan <caden.tann@gmail.com>
2017-06-04 17:17:13 -0400

    basic lightbox completed

--- js/main.js
+++ js/main.js
@@ -12,8 +12,6 @@ $mobileBar.on('click', function() {
 });
 
 
-
-
 /* ================================= 
   Parallax
 ==================================== */
@@ -71,6 +69,137 @@ function moveProgressBar() {
 		);
 	});
 }
+
+
+/* ================================= 
+  Portfolio Lightbox
+==================================== */
+
+// Dynamic elements
+var $overlay = $("<div id='overlay'></div>");
+var $slideContainer = $("<div class='slideContainer'></div>");
+var $image = $("<img>");
+var $content = $("<div class='content'></div>");
+var $nextBtn = $("<button class='next-btn'><span>></span></button>");
+var $prevBtn = $("<button class='prev-btn'><span><</span></button>");
+var $closeBtn = $("<button class='close-btn'>X Close</button>");
+
+// Variables
+var gallery = $('.gallery');
+var projectLink = $('.project a');
+var currentSlide = 0;
+
+// Append dynamic elements
+$slideContainer.append($closeBtn);
+$slideContainer.append($nextBtn);
+$slideContainer.append($prevBtn);
+$slideContainer.append($image);
+$slideContainer.append($content);
+$overlay.append($slideContainer);
+$("body").append($overlay);
+
+// Projects array
+var projects = [
+	{
+		imageUrl: "images/project1.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		imageUrl: "images/project2.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		imageUrl: "images/project3.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		imageUrl: "images/project4.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		imageUrl: "images/project5.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		imageUrl: "images/project6.jpg",
+		title: "project title",
+		description: "Lorem Ipsum has been the industry's standards, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	}
+]
+
+// Print html
+function printHtml() {
+	var html = "";
+	// get each image and descriptionription from array
+	$.each( projects, function( key, value ) {
+		// console.log(key)
+		html += "<li class='project'>" ;		
+		html += "<a href='"+ value.imageUrl + "'" + ">";
+		html += "<img src='" + value.imageUrl + "'" + ">";
+		html += "</a>";		
+		html += "<figcaption class='project-title'>";
+		html += "<p>" + value.title + "</p>";
+		html += "</figcaption>";
+		html += "</li>";
+	});
+	$(gallery).html(html);	
+}
+printHtml();
+
+// Display slide content
+function showSlide() {
+	var index = currentSlide;
+	var slideImage = projects[index].imageUrl;
+	var slideDescription = projects[index].description;
+	// console.log("current " + index)
+	$overlay.show();
+	$image.attr('src', slideImage);
+	$content.html("<p>" + slideDescription + "</p>");
+}
+
+// Show overlay on click event
+$('.gallery li').on('click', projectLink, function( event ){
+	event.preventDefault();
+	// console.log($(this).index());	
+	currentSlide = $(this).index();
+	showSlide();
+});
+
+// Close lightbox when click on close button
+$closeBtn.on('click', function() {
+	$overlay.hide();
+});
+
+$nextBtn.on('click', function(){
+	currentSlide++;
+	if( currentSlide >= projects.length ) {
+		console.log("over")
+		currentSlide = 0;
+	}
+	showSlide();
+});
+
+$prevBtn.on('click', function(){
+	currentSlide--;
+	console.log(currentSlide)
+	if( currentSlide <= -1 ) {
+		console.log("over")
+		currentSlide = projects.length - 1;
+	} 
+	showSlide();
+});
+
+
+
+
+
+
+
 
 
 
